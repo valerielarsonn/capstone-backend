2dc8a0b87a7ddfcc9a9a6c06d11ce495ea2cb747
Valerie Larson <valerielarsonn@gmail.com>
2021-07-17 06:09:53 -0600

    editing last things

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
